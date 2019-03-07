4a28a2faf1c8ace8731400b89fdfeaf0e5e7169a
Christina Williams <christinaxtwilliams@gmail.com>
2019-03-07 15:55:27 +0000

    Automatically backed up by Learn

--- db/migrate/20190307155259_create_figure_titles.rb.rb
+++ db/migrate/20190307155259_create_figure_titles.rb.rb
@@ -1,4 +1,8 @@
 class CreateFigureTitles.rb < ActiveRecord::Migration
   def change
+    create_table :figure_titles do |i|
+      i.integer :title_id
+      i.integer :figure_id
+    end
   end
 end
